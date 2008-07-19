963aaf73ce84e3595875a463f7440d9298102a7c
Ismo Aulaskari <ismo@aulaskari.com>
2008-07-19 16:48:20 +0000

    peli.jar-projektien merge, toivottavasti kaikki toimivia versioita

---
+++ ResultBot/src/resultbot/Main.java
@@ -0,0 +1,119 @@
+package resultbot;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+import java.util.StringTokenizer;
+import org.jibble.pircbot.*;
+
+public class Main extends Thread {
+
+    ResultBot bot;
+    String channel;
+    String headline;
+    String file;
+    String mode;
+    
+    public Main(ResultBot bot, String channel, String headline, String file, String mode) {
+        this.bot = bot;
+        this.channel = channel;
+        this.headline = headline;
+        this.mode = mode;
+        
+        this.bot.joinChannel(this.channel);
+        
+        this.bot.sendMessage0(this.channel, "*" + this.headline + "*");
+        if(file != null) {
+                this.file = file;
+                this.bot.sendMessage0(this.channel, "*" + this.file + "*");
+        }
+    }
+    
+    
+    public void run() {
+        try {
+          if(this.file != null) {                
+                while(true) {
+                  this.bot.pollFile(this.file);
+                  
+                  if(this.mode.equalsIgnoreCase("tnmt")) {
+                    this.bot.sendTnmtFilteredStream(this.channel, this.bot.readNamedFile(this.file));
+                  }
+                  else {
+                      if(this.mode.equalsIgnoreCase("playoff")) {                  
+                        this.bot.sendPlayoffFilteredStream(this.channel, this.bot.readNamedFile(this.file));
+                      }
+                  }
+                  
+                  this.sleep(10);
+                }
+                
+          }
+          else
+                bot.sendUnfilteredStream(   channel, 
+                        new BufferedReader(new 
+                            InputStreamReader(System.in)));
+        } catch (InterruptedException ie) {
+          return;
+        } catch (Exception e) {
+                System.err.println("Give correct input filename as parameter instead of " + this.file + " .\n" + e);
+        }
+        
+    }
+
+    
+    public static void main(String[] args) throws Exception {
+        
+        String conffile = "irc.properties";
+        
+        Properties props = new Properties();
+        try {
+            FileInputStream in = new FileInputStream( conffile );
+            props.load(in);
+        } catch (FileNotFoundException fe) {
+            System.err.println("Missing " + conffile + " -file!");
+            System.exit(-1);
+        } catch (IOException ie) {
+            System.err.println("Reading " + conffile + " failed!");
+            System.exit(-1);
+        }
+
+        String name = props.getProperty("name");
+        String login = props.getProperty("login");
+        String finger = props.getProperty("finger");
+        String server = props.getProperty("server");
+        String channel = props.getProperty("channel");
+        String headline = props.getProperty("headline");
+        
+        // Now start our bot up.
+        ResultBot bot = new ResultBot(name, login, finger);
+
+        // Enable debugging output.
+        bot.setVerbose(true);
+        
+        // try to talk slow and avoid being kicked
+        bot.setMessageDelay(500);
+        
+        // Connect to the IRC server.
+        bot.connect(server);
+        
+        if(args.length > 0) {            
+            StringTokenizer st = new StringTokenizer(args[0], ".");
+            
+            if(st.countTokens() == 2) {
+              String filename = st.nextToken();
+              String extension = st.nextToken();
+              if(extension.equalsIgnoreCase("tnmt")) { 
+                //expecting both a tnmt and a plaoff-txt-file                  
+                  new Main(bot, channel, headline, filename + ".txt", "playoff").start();    
+              }                                               
+            }
+            //general tnmt-file reader
+            new Main(bot, channel, headline, args[0], "tnmt").start();
+        }
+        //std input -reader
+        new Main(bot, channel, headline, null, "none").start();
+    }
+}
---
+++ ResultBot/src/resultbot/ResultBot.java
@@ -0,0 +1,248 @@
+/*
+ * ResultBot.java
+ * 
+ * Created on 16/10/2007, 23:53:14
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package resultbot;
+
+/**
+ *
+ * @author aulaskar
+ */
+        
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.StringTokenizer;
+import java.util.Vector;
+import org.jibble.pircbot.*;
+
+public class ResultBot extends PircBot {
+    
+    String version = "0.07";
+    private boolean isFirstRun = true;
+    private boolean stop = false;
+    private boolean resultsFound = false;
+    private String lastLine = null;
+    private int lastRound = 0;
+    private int currentRound = 1;
+    private long lastPrintedLine = 0;
+    private long currentLine = 0;
+    private ArrayList playoffs = new ArrayList();
+    
+    public ResultBot() {
+        this.setName("Tulospalvelu");
+        this.setLogin("tunne");
+        this.setFinger("Tuloksia reaalisti!");
+        this.setVersion(version);
+    }
+    
+    public ResultBot(String name, String login, String finger) {
+        this.setName(name);
+        this.setLogin(login);
+        this.setFinger(finger);
+        this.setVersion(version);
+    }
+    
+    
+    /** this will loop until file is modified */
+    public void pollFile(String filename) throws IOException, InterruptedException {
+        File f = new File(filename);
+        Thread.sleep(1000);
+        if(f.exists() && f.canRead()) {
+            Date d = new Date();
+            Date d2 = new Date();
+            d.setTime(f.lastModified());
+            while(true) {
+                d2.setTime(f.lastModified());
+                if(d.compareTo(d2) != 0) {
+                    this.currentLine = 0;
+                    this.stop = false;
+                    this.resultsFound = false;
+                    return;
+                }
+                Thread.sleep(1000);    
+            }
+            
+        } else {
+            throw new IOException("Input file can't be read.");
+        }
+            
+    }
+    
+    
+    
+    public BufferedReader readNamedFile(String filename) {
+        BufferedReader is = null;
+        try {
+           is = new BufferedReader(
+                    new FileReader(filename)
+                    );
+            String inputLine;
+            
+            
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        
+        return is;
+    }
+
+    
+    /** send unfiltered input stream */
+    public void sendUnfilteredStream(String channel, BufferedReader is) {
+        try {
+            String inputLine;
+            
+            while ((inputLine = is.readLine()) != null) {
+                this.sendMessage0(channel, inputLine);
+            }
+            is.close();
+            
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        
+        return;
+    }
+
+    
+    /** send tnmt-file filtered by tnmt filter */
+    public void sendTnmtFilteredStream(String channel, BufferedReader is) {
+        try {
+            String inputLine;
+            
+            while ((inputLine = is.readLine()) != null) {
+                inputLine = filterTnmtLine(inputLine);
+                if(inputLine != null ) 
+                    this.sendMessage0(channel, inputLine);                    
+            }
+            is.close();
+            
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        
+        return;
+    }
+
+    /** send txt-file filtered by playoff filter */
+    public void sendPlayoffFilteredStream(String channel, BufferedReader is) {
+        try {
+            String inputLine;
+            int i = 0;
+            
+            while ((inputLine = is.readLine()) != null) {
+                if(isFirstRun) {
+                    this.playoffs.add(i, inputLine);                    
+                } 
+                else {
+                    //print only changed lines
+                    if(this.playoffs.size() > i && this.playoffs.get(i).equals(inputLine)) {
+                        inputLine = null;
+                    }
+                    else {
+                        if(this.playoffs.size() > i)
+                          this.playoffs.set(i, inputLine);
+                        else
+                          this.playoffs.add(i, inputLine);
+                    }
+                }
+                i++;                
+                inputLine = filterPlayoffLine(inputLine);
+                
+                if(inputLine != null ) 
+                    this.sendMessage0(channel, "Playoff: " + inputLine);                    
+            }
+            is.close();
+            this.isFirstRun = false;
+            
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        
+        return;
+    }
+
+    
+    /** print lines relevant to playoff matches */
+    public String filterPlayoffLine(String inputLine) {       
+        String token = null;
+                    
+        if(inputLine == null ) 
+            return null;
+        
+        StringTokenizer st = new StringTokenizer(inputLine, "-");
+        //not a match
+        if(st.countTokens() < 2) {
+            return null;
+        }
+                
+        return inputLine;
+    }
+    
+    
+    /** choose tnmt lines to print or to ignore */
+    public String filterTnmtLine(String inputLine) {
+        this.currentLine++;
+        String token = null;
+        
+        
+        if(this.currentLine > this.lastPrintedLine) {
+            if(this.stop) {
+                return null;
+            }            
+        
+            StringTokenizer st = new StringTokenizer(inputLine, ":");            
+            if(st.countTokens() >= 4) {
+                this.resultsFound = true;
+                //this.stop = true;
+            }
+            else {
+                if(this.resultsFound) {
+                    this.stop = true;  
+                }
+            }
+            
+            token = st.nextToken();
+            if(this.isFirstRun) {
+                if(token.equalsIgnoreCase("rounds")) {
+                    this.stop = true;
+                    this.isFirstRun = false;
+                }
+            }
+            else {
+                if(st.countTokens() < 3 && 
+                        st.countTokens() > 0 && 
+                        ! token.equalsIgnoreCase("ROUND") && 
+                        ! token.equalsIgnoreCase("END-OF-ROUND")) {
+                    inputLine = "Odotamme tulosta " + inputLine;
+                    this.stop = true;
+                    return null;
+                }
+            }
+            
+            this.lastPrintedLine++;                               
+        } 
+        else {
+            return null;
+        }
+        
+        return inputLine;
+    }
+
+    
+    /** print wrapper for testing purposes */
+    public void sendMessage0(String channel, String message) {
+        //System.out.println(channel+ " " +message);
+        this.sendMessage(channel, message);
+    }
+}
