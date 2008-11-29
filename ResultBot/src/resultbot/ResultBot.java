3b5304142b6561bf77d48a505bccac2b670adc8f
Ismo Aulaskari <ismo@aulaskari.com>
2008-11-29 19:19:50 +0000

    cleanup

--- ResultBot/src/resultbot/ResultBot.java
+++ ResultBot/src/resultbot/ResultBot.java
@@ -18,11 +18,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.StringTokenizer;
-import java.util.Vector;
 import org.jibble.pircbot.*;
 
 public class ResultBot extends PircBot {
