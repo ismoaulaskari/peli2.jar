1e71d26b3340617c1a650de9b81b60ffcfbb5f21
Ismo Aulaskari <ismo@aulaskari.com>
2008-12-04 13:19:05 +0000

    playoff-taulumallia tekemään

--- src/peli/PlayoffPair.java
+++ src/peli/PlayoffPair.java
@@ -2,40 +2,101 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package peli;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author aulaskar
  */
 public class PlayoffPair {
-        
     //rounds
+    private String homeTeam,  awayTeam;
+    private int homeWins,  awayWins;
     private Playoff mother;
     private ArrayList matches = new ArrayList();
-    
-    
+
     public PlayoffPair(Playoff playoff, String homeTeam, String awayTeam) {
-        if(playoff == null) {
+        if (playoff == null) {
             throw new IllegalArgumentException("Empty playoffs attempted!");
         }
-        
+
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
         this.mother = playoff;
     }
 
+    public int getWins(String player) {
+        if (player.equals(getHomeTeam())) {
+            return this.getHomeWins();
+        } else if (player.equals(getAwayTeam())) {
+            return this.getAwayWins();
+        }
 
-    
+        return 0;
+    }
 
-    public List getWinner() {
-        return null;
-    }
-    
-    public List getLoser() {
-        return null;
-    }
+    public void updateWins() {
+        this.homeWins = 0;
+        this.awayWins = 0;
         
+        for (int i = 0; i < this.getMatches().size(); i++) {
+            Match match = (Match) this.getMatches().get(i);
+            if (match.homeGoals() > match.visitorGoals()) {
+                this.homeWins++;
+            } else if (match.homeGoals() < match.visitorGoals()) {
+                this.awayWins++;
+            }
+        }
+                
+    }
+
+    public String getWinner() {
+        if(this.getHomeWins() > this.getAwayWins()) return this.getHomeTeam();
+        else if(this.getHomeWins() < this.getAwayWins()) return this.getAwayTeam();
+        
+        return null;
+    }
+
+    public String getLoser() {
+        if(this.getHomeWins() < this.getAwayWins()) return this.getHomeTeam();
+        else if(this.getHomeWins() > this.getAwayWins()) return this.getAwayTeam();
+
+        return null;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public ArrayList getMatches() {
+        return matches;
+    }
+
+    public void setMatches(ArrayList matches) {
+        this.matches = matches;
+    }
+
+    public int getHomeWins() {
+        return homeWins;
+    }
+
+    public int getAwayWins() {
+        return awayWins;
+    }
+    
+    
 }
--- src/peli/PlayoffPairTableModel.java
+++ src/peli/PlayoffPairTableModel.java
@@ -3,7 +3,6 @@ package peli;
 // Jad home page: http://www.geocities.com/kpdus/jad.html
 // Decompiler options: packimports(3) 
 // Source File Name:   RoundTableModel.java
-
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javax.swing.table.AbstractTableModel;
@@ -14,105 +13,105 @@ import javax.swing.table.AbstractTableModel;
  * @author aulaskar
  *
  */
-public class PlayoffPairTableModel extends AbstractTableModel
-{
+public class PlayoffPairTableModel extends AbstractTableModel {
+
     private ResourceBundle messages = Constants.getInstance().getMessages();
-    
-    final String columnNames[] = {        
+    final String columnNames[] = {
         messages.getString("playerName"), messages.getString("playoffWins"),
         messages.getString("playoffResults")
     };
-    
-    private String homeTeam;
-    private String awayTeam;
-    private Playoff matches;
-
-    PlayoffPairTableModel(PlayoffPair playoffpair)
-    {
-        this.homeTeam = homeTeam;
-        this.awayTeam = awayTeam;
-        this.matches = matches;
+    private PlayoffPair playoffpair;
+    //private String homeTeam;
+    //private String awayTeam;
+    //private ArrayList matches;
+    PlayoffPairTableModel(PlayoffPair playoffpair) {
+        this.playoffpair = playoffpair;
+//        this.homeTeam = playoffpair.getHomeTeam();
+//        this.awayTeam = playoffpair.getAwayTeam();
+//        this.matches = playoffpair.getMatches();
     }
 
-    public int getColumnCount()
-    {
+    public int getColumnCount() {
         return columnNames.length;
     }
 
-    public int getRowCount()
-    {
+    public int getRowCount() {
         return 2;
     }
 
-    public String getColumnName(int i)
-    {
+    public String getColumnName(int i) {
         return columnNames[i];
     }
 
-    public Object getValueAt(int i, int j)
-    {
-        Match match = matches.getMatch(i);
-        switch(j)
-        {
-        case 0: // '\0'
-            return "" + (i + 1);
+    public Object getValueAt(int i, int j) {
+        Match match = (Match) playoffpair.getMatches().get(i-2);
+        switch (i) {
+            case 0:
+                if (j == 0) {
+                    return playoffpair.getHomeTeam();
+                } else {
+                    return playoffpair.getAwayTeam();
+                }
 
-        case 1: // '\001'
-            return match.home();
+            case 1:
+                if (j == 0) {
+                    return playoffpair.getHomeWins();
+                } else {
+                    return playoffpair.getAwayWins();
+                }
 
-        case 2: // '\002'
-            return match.visitor();
-
-        case 3: // '\003'
-            if(isDummyMatch(i))
-                return "xxx";
-            else
-                return match.getResult();
+            case 2:
+                if (j == 0) {
+                    if (isDummyMatch(i)) {
+                        return "xxx";
+                    } else {
+                        return match.getResult();
+                    }
+                }
+                else {
+                    return "";
+                }
         }
         return "";
     }
 
-    public Class getColumnClass(int i)
-    {
+    public Class getColumnClass(int i) {
         return getValueAt(0, i).getClass();
     }
 
-    public boolean isCellEditable(int i, int j)
-    {
-        if(j < 3)
+    public boolean isCellEditable(int i, int j) {
+        if (j < 3) {
             return false;
+        }
         return !isDummyMatch(i);
     }
 
-    private boolean isDummyMatch(int i)
-    {
-        Match match = round.getMatch(i);
+    private boolean isDummyMatch(int i) {
+        Match match = (Match) playoffpair.getMatches().get(i-2);
         String s = match.home();
         String s1 = match.visitor();
         return s.equals("X") || s1.equals("X");
     }
 
-    public void setValueAt(Object obj, int i, int j)
-    {
-        if(j != 3)
+    public void setValueAt(Object obj, int i, int j) {
+        if (j != 3) {
             return;
+        }
         SaveTracker.isSaved = false;
-        Match match = round.getMatch(i);
+        Match match = (Match) playoffpair.getMatches().get(i-2);
         SeriesTableEntry seriestableentry = round.getDivision().getSeriesTableEntry(match.home());
         SeriesTableEntry seriestableentry1 = round.getDivision().getSeriesTableEntry(match.visitor());
-        if(match.isOver())
-        {
+        if (match.isOver()) {
             seriestableentry.cancelMatch(match);
             seriestableentry1.cancelMatch(match);
         }
-        match.setResult((String)obj);
+        match.setResult((String) obj);
         seriestableentry.updateWith(match);
         seriestableentry1.updateWith(match);
         fireTableCellUpdated(i, j);
     }
 
-    public void finalize() {        
-        this.round = null;        
+    public void finalize() {
+        this.playoffpair = null;
     }
-
 }
