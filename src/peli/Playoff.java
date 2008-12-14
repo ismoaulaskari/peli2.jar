05629b3de85002316c3e632254188d75bbe76264
Ismo Aulaskari <ismo@aulaskari.com>
2008-12-14 00:48:26 +0000

    käyttöliittymää idiooteille

--- src/peli/Playoff.java
+++ src/peli/Playoff.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package peli;
 
 import java.util.ArrayList;
@@ -13,16 +12,15 @@ import java.util.List;
  * @author aulaskar
  */
 public class Playoff {
-    
+
     private int size;
     private List players;
     //rounds
     private Tournament mother;
     private PlayoffPair[] playoffPairs;
-    
-    
+
     public Playoff(Tournament tournament, List players, int size) {
-        if(players == null || size == 0) {
+        if (players == null || size == 0) {
             throw new IllegalArgumentException("Empty playoffs attempted!");
         }
         this.players = players;
@@ -34,21 +32,32 @@ public class Playoff {
     public Playoff(Tournament tournament, List players) {
         this(tournament, players, players.size());
     }
-    
     //won't support empty players
     private PlayoffPair[] createPairs(List players, int size) {
-        if(size > players.size()) {
+        if (size > players.size()) {
             size = players.size();
         }
-        PlayoffPair[] pairs = new PlayoffPair[size/2];
-        for(int i=0; i<size/2; i++) {
-            //pairs[i] = new PlayoffPair(this, "foo"+i, "bar"+i);        
-            pairs[i] = new PlayoffPair(this, (String)players.remove(0), (String)players.remove(0));        
+        PlayoffPair[] pairs = new PlayoffPair[size / 2];
+        for (int i = 0; i < size / 2; i++) {            
+            String player1, player2;
+            
+            if (!players.isEmpty()) {
+                player1 = (String) players.remove(0);
+            } else {
+                player1 = "X";
+            }
+            if (!players.isEmpty()) {
+                player2 = (String) players.remove(0);
+            } else {
+                player2 = "X";
+            }            
+
+            pairs[i] = new PlayoffPair(this, player1, player2);
         }
-                
+
         return pairs;
     }
-    
+
     public int getSize() {
         return size;
     }
@@ -67,24 +76,23 @@ public class Playoff {
 
     public List getSurvivors() {
         ArrayList survivors = new ArrayList();
-        for(int i=0; i<playoffPairs.length; i++) {
+        for (int i = 0; i < playoffPairs.length; i++) {
             survivors.add(playoffPairs[i].getWinner());
         }
-        
+
         return survivors;
     }
-    
+
     public List getLosers() {
         ArrayList losers = new ArrayList();
-        for(int i=0; i<playoffPairs.length; i++) {
+        for (int i = 0; i < playoffPairs.length; i++) {
             losers.add(playoffPairs[i].getWinner());
         }
-        
+
         return losers;
     }
 
     public PlayoffPair[] getPlayoffPairs() {
         return playoffPairs;
     }
-        
 }
