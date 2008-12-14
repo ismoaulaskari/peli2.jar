a0004912e08cadfcb8db5ece903f117df894a835
Ismo Aulaskari <ismo@aulaskari.com>
2008-12-14 21:50:41 +0000

    jatkoaikatukea tekemään

--- src/peli/Match.java
+++ src/peli/Match.java
@@ -3,7 +3,6 @@ package peli;
 // Jad home page: http://www.geocities.com/kpdus/jad.html
 // Decompiler options: packimports(3) 
 // Source File Name:   Match.java
-
 import java.io.PrintWriter;
 
 /**
@@ -11,141 +10,140 @@ import java.io.PrintWriter;
  * @author aulaskar
  *
  */
-public class Match
-{
+public class Match {
 
     private String home;
     private String visitor;
     private int homeGoals;
     private int visitorGoals;
     private boolean isOver;
+    private String postFix; //overtime? disqualification?
 
-	//for England..and mutual comparison
-	Match(String home, String visitor) 
-	{
-		this.home = home;
-		this.visitor = visitor;
-		isOver = false;
-	}
-	
+    //for England..and mutual comparison
+    Match(String home, String visitor) {
+        this.home = home;
+        this.visitor = visitor;
+        isOver = false;
+        this.postFix = "";
+
+    }
+
     Match(String s)
-        throws FileFormatException
-    {
+            throws FileFormatException {
         isOver = false;
         int i = s.indexOf(':');
-        if(i > 0)
-        {
+        if (i > 0) {
             home = s.substring(0, i);
             s = s.substring(i + 1);
-        } else
-        {
+        } else {
             throw new FileFormatException();
         }
         i = s.indexOf(':');
-        if(i > 0)
-        {
+        if (i > 0) {
             visitor = s.substring(0, i);
             s = s.substring(i + 1);
-        } else
-        {
+        } else {
             visitor = s;
             return;
         }
         i = s.indexOf(':');
-        if(i > 0)
-        {
+        if (i > 0) {
             homeGoals = Integer.parseInt(s.substring(0, i));
             visitorGoals = Integer.parseInt(s.substring(i + 1));
             isOver = true;
+            s = s.substring(i + 1);
+        }
+        i = s.indexOf(':');
+        if (i > 0) {
+            postFix = s;
         }
     }
 
-    public String getHtmlTableRow(int i)
-    {
+    public String getHtmlTableRow(int i) {
         String s = isOver ? "" + homeGoals : "&nbsp;&nbsp;&nbsp;";
         String s1 = isOver ? "" + visitorGoals : "&nbsp;&nbsp;&nbsp;";
         return "<TR><TD>" + i + "</TD>" + "<TD>" + ":" + "</TD>" + "<TD>" + home + "</TD>" + "<TD ALIGN=CENTER>" + "-" + "</TD>" + "<TD>" + visitor + "</TD>" + "<TD ALIGN=CENTER>" + s + "</TD>" + "<TD ALIGN=CENTER>" + "-" + "</TD>" + "<TD ALIGN=CENTER>" + s1 + "</TD>" + "</TR>";
     }
 
-    public void print()
-    {
+    public void print() {
         System.out.println(home + " - " + visitor);
     }
 
-    public void save(PrintWriter printwriter)
-    {
+    public void save(PrintWriter printwriter) {
         printwriter.print(home + ":" + visitor);
-        printwriter.println(isOver ? ":" + homeGoals + ":" + visitorGoals : "");
+        printwriter.println(isOver ? ":" + homeGoals + ":" + visitorGoals : "" + (postFix.length() > 0 ? ":" + postFix : ""));
+    //printwriter.println(isOver ? ":" + homeGoals + ":" + visitorGoals : "");
     }
 
-    public String getResult()
-    {
-        if(!isOver)
+    public String getResult() {
+        if (!isOver) {
             return "";
-        else
+        } else {
             return homeGoals + "-" + visitorGoals;
+        }
     }
 
-    public String getResultInverted()
-    {
-        if(!isOver)
+    public String getResultInverted() {
+        if (!isOver) {
             return "";
-        else
+        } else {
             return visitorGoals + "-" + homeGoals;
+        }
     }
 
-    public void setResult(String s)
-    {
+    public void setResult(String s) {
         s = s.trim();
-        if(s.equals(""))
-        {
+        if (s.equals("")) {
             isOver = false;
             return;
         }
         int i = s.indexOf('-');
-        if(i > 0)
-            try
-            {
+        if (i > 0) {
+            try {
                 homeGoals = Integer.parseInt(s.substring(0, i));
-                visitorGoals = Integer.parseInt(s.substring(i + 1));
-                isOver = true;
-            }
-            catch(NumberFormatException numberformatexception)
-            {
+                //visitorGoals = Integer.parseInt(s.substring(i + 1));
+                s = s.substring(i + 1);
+                String[] endOfLine = s.split(":", 2);
+                if (endOfLine.length > 0) {
+                    visitorGoals = Integer.parseInt(endOfLine[0]);
+                    isOver = true;
+                } else {
+                    isOver = false;
+                }
+                if (endOfLine.length > 1) {
+                    postFix = endOfLine[1];
+                }
+            } catch (NumberFormatException numberformatexception) {
+                isOver = false;
+            } catch (ArrayIndexOutOfBoundsException arrayException) {
                 isOver = false;
             }
-        else
+        } else {
             isOver = false;
+        }
     }
 
-    public String home()
-    {
+    public String home() {
         return home;
     }
 
-    public String visitor()
-    {
+    public String visitor() {
         return visitor;
     }
 
-    public int homeGoals()
-    {
+    public int homeGoals() {
         return homeGoals;
     }
 
-    public int visitorGoals()
-    {
+    public int visitorGoals() {
         return visitorGoals;
     }
 
-    public boolean isOver()
-    {
+    public boolean isOver() {
         return isOver;
     }
 
-    public boolean isDummy()
-    {
+    public boolean isDummy() {
         return home.equals("X") || visitor.equals("X") || home.equals("") || visitor.equals("");
     }
-
 }
