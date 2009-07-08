6d8534b439e22f47155cb8e496b11321ea36e4f4
Ismo Aulaskari <ismo@aulaskari.com>
2009-07-08 11:32:33 +0000

    toimii jotenkuten

--- ResultBot/src/resultbot/ResultBot2.java
+++ ResultBot/src/resultbot/ResultBot2.java
@@ -17,6 +17,7 @@ public class ResultBot2 extends ResultBot {
 
     String version = "0.20";
     ArrayList<String> tnmtMemory = new ArrayList<String>();
+    private boolean isPlayoff = false;
 
     public ResultBot2(String name, String login, String finger) {
         super(name, login, finger);
@@ -32,13 +33,14 @@ public class ResultBot2 extends ResultBot {
     public void sendTnmtFilteredStream(String channel, BufferedReader is) {
         try {
             String inputLine;
-            int currentLine = 0;
+            int lineNumber = 0;
             while ((inputLine = is.readLine()) != null) {
-                inputLine = pipeOnlyIfChanged(filterTnmtLine3(inputLine), currentLine++);
+                inputLine = pipeOnlyIfChanged(smartFilterTnmtLine(inputLine), lineNumber++);
                 if (inputLine != null) {
                     this.sendMessage0(channel, inputLine);
                 }
             }
+            this.isPlayoff = false;
             is.close();
         } catch (IOException e) {
             System.out.println("IOException: " + e);
@@ -47,8 +49,12 @@ public class ResultBot2 extends ResultBot {
     }
 
     /** choose tnmt lines to print or to ignore */
-    public String filterTnmtLine3(String inputLine) {
+    public String filterDivisionTnmtLine(String inputLine) {
         this.currentLine++;
+
+        if (inputLine.matches("^END-OF.*")) {
+            return null;
+        }
 
         //only print new lines
         if ((this.currentLine > this.lastPrintedLine) && (this.stop == false)) {
@@ -72,47 +78,84 @@ public class ResultBot2 extends ResultBot {
                 else if (this.resultsFound) {
                     this.stop = true;
                 } else { //too far ahead (someone pressed save)?
-                    if (inputLine.matches("^ROUND:")) {
-                        return inputLine;
-                    } else {
-                        if (!this.isFirstRun) {
-                            return null;
-                        }
+
+                    if (inputLine.matches("^PLAYOFFS-SIZE:.*")) {
+                        this.isPlayoff = true;
                     }
+
+                    if (inputLine.matches("^ROUND:.*")) {
+                        
+                        return inputLine;
+                    }
+
+                    if (!this.isFirstRun) {
+                        return null;
+                    }
+
                 }
             }
 
-            if (inputLine.matches("^END-OF")) {
-                return null;
-            } else {
-                return inputLine;
-            }
+            return inputLine;
         }
 
         return null;
     }
 
-    /*
-    public String filterTnmtLine(String input) {
-    return input;
-    }
+    /**
+     * print larger picture
+     * @param inputLine
+     * @return
      */
+    private String filterPlayoffTnmtLine(String inputLine) {
+        if (inputLine.matches("^END-OF.*")) {
+            return null;
+        }
+
+        if(inputLine.matches("^PLAYOFF-SIZE:.*")) {
+            return inputLine;
+        }
+
+        //empty result
+        if (inputLine.matches("^[^:]+:[^:]+$")) {
+            return null;
+        }
+
+
+       if (this.isPlayoff) {
+            return inputLine;
+        } else {
+            return null;
+        }
+        
+    }
+
+    public String smartFilterTnmtLine(String input) {
+        if (this.isPlayoff) {
+            input = filterPlayoffTnmtLine(input);
+        } else {
+            input = filterDivisionTnmtLine(input);
+        }
+
+        return input;
+    }
+
     private String pipeOnlyIfChanged(String inputLine, int lineNumber) {
-        /*if(this.tnmtMemory.size() > lineNumber &&
-        this.tnmtMemory.get(lineNumber).equalsIgnoreCase(inputLine)) {
-        //line has not changed
-        inputLine = null;
-        }
-        else {
-        try {
-        this.tnmtMemory.remove(lineNumber);
-        this.tnmtMemory.add(lineNumber, "Aa" + lineNumber + inputLine);
-        }
-        catch(IndexOutOfBoundsException ie) {
-        //ok
-        }
-        }
-         */
         return inputLine;
+        /*
+        if (this.tnmtMemory.size() > lineNumber &&
+                this.tnmtMemory.get(lineNumber).equalsIgnoreCase(inputLine)) {
+            //line has not changed
+            System.out.println(this.tnmtMemory.get(lineNumber) + "=" + inputLine);
+            inputLine = null;
+        } else {
+            try {
+                System.out.print("REMOVE" + this.tnmtMemory.remove(lineNumber));
+                this.tnmtMemory.add(lineNumber, inputLine);
+            } catch (IndexOutOfBoundsException ie) {
+                //ok
+            }
+        }
+
+        return inputLine;*/
     }
 }
