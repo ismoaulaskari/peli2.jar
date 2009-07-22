cd01eb5e541842ca95dd05557274078f4f3f124d
Ismo Aulaskari <ismo@aulaskari.com>
2009-07-22 12:58:40 +0000

    Blackbox-testauksen alku

---
+++ testit/testaa.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd ../dist && \
+cp ../testit/genhtml/*tnmt .
+for TNMT in `ls *tnmt` 
+do
+  echo "Luetaan $TNMT ja generoidaan ${TNMT}.html"
+  java -jar peli2.jar $TNMT headless >"${TNMT}.html"
+  diff "${TNMT}.html" "../testit/genhtml/${TNMT}.html"
+done
