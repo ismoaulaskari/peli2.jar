05676614fae79107fa152b4c2be4e56fb3e4413a
Ismo Aulaskari <ismo@aulaskari.com>
2009-08-19 19:21:08 +0000

    debug-printti

--- src/peli/LiveResults.java
+++ src/peli/LiveResults.java
@@ -4,10 +4,12 @@
  */
 package peli;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Authenticator;
 import java.util.ResourceBundle;
 import java.util.TimerTask;
@@ -114,9 +116,37 @@ public class LiveResults extends TimerTask {
         this.postRequest.connect();
         InputStream serverReply = this.postRequest.post();
         if (serverReply != null) {
-            reply = serverReply.toString();
+            reply = convertStreamToString(serverReply);
             serverReply.close();
         }
         return reply;
+    }
+
+    public String convertStreamToString(InputStream is) {
+        /*
+         * To convert the InputStream to String we use the BufferedReader.readLine()
+         * method. We iterate until the BufferedReader return null which means
+         * there's no more data to read. Each line will appended to a StringBuilder
+         * and returned as String.
+         */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
     }
 }
